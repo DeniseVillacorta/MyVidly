6b990a30735445ef57dc13863cdab7b45d34fae5
Denise Villacorta <villacorta9806denise@gmail.com>
2020-08-02 17:35:16 +0800

    Enable Migrations

---
+++ Domain/DatabaseContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Vidly.Domain
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base(@"Data Source=localhost\SQLEXPRESS; Initial Catalog=MyVidly;Trusted_Connection=True;")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
