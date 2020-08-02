fac5830f4887ff0794ad64c391ad27aad1f7e816
Denise Villacorta <villacorta9806denise@gmail.com>
2020-08-02 16:20:56 +0800

    Added Customer Model and Random ViewModel

--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModel;
 
 namespace Vidly.Controllers
 {
@@ -12,7 +13,19 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie { MovieName = "Harry Potter" };
-            return View(movie);
+            var customer = new List<Customer>
+            {
+                new Customer {CustomerName = "Denise Villacorta"},
+                new Customer {CustomerName = "Ravni Arador"},
+            };
+
+            var viewModel = new RandomViewModel
+            {
+                Movie = movie,
+                Customers = customer
+            };
+            return View(viewModel);
+          
         }
         // GET: Movie
         public ActionResult Index()
