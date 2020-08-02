c50655a16e8c843c4afa44e68e659eb06b074887
Denise Villacorta <villacorta9806denise@gmail.com>
2020-08-02 20:08:39 +0800

    Add birthday to Customer

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -15,5 +16,6 @@ namespace Vidly.Models
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
         public byte MembershipTypeId { get; set; }
+        public DateTime? CustomerBday { get; set; }
     }
 }
