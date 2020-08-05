3dfdbaa4e22e98e10f741f8b2b34cf83640b262e
Denise Villacorta <villacorta9806denise@gmail.com>
2020-08-05 14:19:19 +0800

    Added Phone User Profile in Registration

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -154,7 +154,10 @@ namespace Vidly.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser
+                {   UserName = model.Email, 
+                    PhoneNumber = model.Phone,
+                    Email = model.Email };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
