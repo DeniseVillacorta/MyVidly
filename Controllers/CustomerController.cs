0d7b705ceb689fb3ebda1839096890c4b3432c22
Denise Villacorta <villacorta9806denise@gmail.com>
2020-08-07 11:05:49 +0800

    Disable session state

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Runtime.Caching;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Domain;
@@ -27,6 +28,12 @@ namespace Vidly.Controllers
         [Authorize]
         public ViewResult Index()
         {
+            if (MemoryCache.Default["Genres"] ==null)
+            {
+                MemoryCache.Default["Genres"] = _context.Genre.ToList();
+            }
+
+            var genres = MemoryCache.Default["Genres"] as IEnumerable<Genre>;
             return View();
         }
 
