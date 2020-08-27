079397ef1ad187cf9e23da90c5789c3f0bf99f9e
Denise Villacorta <villacorta9806denise@gmail.com>
2020-08-27 20:51:48 +0800

    Added Custom Error Pages

--- App_Start/Startup.Auth.cs
+++ App_Start/Startup.Auth.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
@@ -55,8 +56,8 @@ namespace Vidly
             //   consumerSecret: "");
 
             app.UseFacebookAuthentication(
-               appId: "908026486362560",
-               appSecret: "56baf74c908fec38bec3bc8c55b78456");
+               appId: ConfigurationManager.AppSettings["FacebookAppId"],
+               appSecret: ConfigurationManager.AppSettings["FacebookAppSecret"]);
 
             //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
             //{
